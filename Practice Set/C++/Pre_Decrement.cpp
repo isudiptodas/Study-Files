cd68120821f1fb428b897e35e09f08cf7716da54
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-03-21 22:14:27 +0530

    Added C++ practice set

---
+++ Practice Set/C++/Pre_Decrement.cpp
@@ -0,0 +1,21 @@
+// Pre decrement (First decrement the value by 1 then print the value)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 6;
+    int b = 18;
+    
+    cout<<"Before decrement "<<endl;
+    cout<<a<<" "<<b<<endl;
+    
+    --a;
+    --b;
+    
+    cout<<"After decrement "<<endl;
+    cout<<a<<" "<<b<<endl;
+    
+    
+}
